41dae63e0ce39db3da03ed69edd22754163063d6
Adarsh Tugaon <E007084@cigniti.com>
2022-10-24 14:08:35 +0530

    metics check

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -72,7 +72,7 @@ def train_and_evaluate(config_path):
             "l1_ratio": l1_ratio,
         }
         json.dump(params, f, indent=4)
-
+############
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.joblib")
     joblib.dump(lr, model_path)
